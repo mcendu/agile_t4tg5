24f6bd548d920f8e0626604269ad161c3c391788
Nathan Du <nathandu@outlook.com>
2023-08-01 09:51:33 +0800

    improve tab edit dialog behavior

--- src/assets/vue/pagelist/edit.vue
+++ src/assets/vue/pagelist/edit.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts">
 import Page from '../../js/page';
-import { onBeforeUpdate, ref, Ref } from 'vue';
+import { computed, ref, Ref, watch } from 'vue';
 
 const props = defineProps<{ page?: Page }>();
 const emit = defineEmits<{
   (e: 'submit', page: Page, name: string): void;
 }>();
 
-let prevPage = props.page;
 const name: Ref<string | undefined> = ref(props.page?.name);
 const dialog: Ref<HTMLDialogElement | null> = ref(null);
 
-onBeforeUpdate(() => {
-  if (prevPage?.id !== props.page?.id) {
-    name.value = props.page?.name;
-    prevPage = props.page;
-  }
-});
+watch(() => props.page?.name, (newName) => {
+  name.value = newName;
+})
 
 function rename(e: Event) {
   if (!props.page) return;
@@ -24,6 +20,10 @@ function rename(e: Event) {
   // validate
   if (name.value) emit('submit', props.page, name.value);
   else e.preventDefault();
+}
+
+function cancel() {
+  name.value = props.page?.name;
 }
 
 function showModal() {
@@ -38,6 +38,7 @@ defineExpose({
 <template>
   <dialog class="sa-dialog" ref="dialog">
     <form method="dialog">
+      <h2 class="form-heading">Editing {{ page?.name }}</h2>
       <p>
         <label>Name</label>
         <input type="text" name="name" v-model="name" />
@@ -46,7 +47,9 @@ defineExpose({
         <button class="form-button submit" type="submit" @click="rename">
           Save
         </button>
-        <button class="form-button" type="submit">Cancel</button>
+        <button class="form-button" type="submit" @click="cancel">
+          Cancel
+        </button>
       </p>
     </form>
   </dialog>
