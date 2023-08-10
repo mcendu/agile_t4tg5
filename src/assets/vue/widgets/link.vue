07b281fd50699fc88e4c2642c42bc8eaa1006cc3
Nathan Du <nathandu@outlook.com>
2023-08-10 10:50:30 +0800

    LinkWidget: fwd delete event to WidgetBase

--- src/assets/vue/widgets/link.vue
+++ src/assets/vue/widgets/link.vue
@@ -24,7 +24,6 @@ const props = defineProps<{
 }>();
 const emit = defineEmits<{
   update: [value: LinkWidgetData];
-  delete: [];
 }>();
 
 const data = computed({
@@ -44,11 +43,7 @@ function urlSummary(link: URL | string) {
 const formState = new FormState(data);
 </script>
 <template>
-  <WidgetBase
-    class="sa-link-widget"
-    @edit="() => formState.reset()"
-    @delete="$emit('delete')"
-  >
+  <WidgetBase class="sa-link-widget" @edit="() => formState.reset()">
     <a class="sa-link-widget__content" :href="data.target">
       <h3 class="sa-link-widget__title">{{ data.title }}</h3>
       <p class="sa-link-widget__link">
