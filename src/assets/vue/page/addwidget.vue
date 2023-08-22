f23d6a3fb86b90f98eb2cd8de61d9d5be84a5246
Csisz <59371699+Csisz@users.noreply.github.com>
2023-08-22 11:20:40 +0200

    add more widgets

--- src/assets/vue/page/addwidget.vue
+++ src/assets/vue/page/addwidget.vue
@@ -1,9 +1,22 @@
 <template>
-  <button class="sa-addwidget" aria-label="Create a new widget">
-    <p class="sa-addwidget__icon">add</p>
+  <button class="sa-addwidget" aria-label="Create a new widget" @mouseover="showIcons = true"
+    @mouseout="showIcons = false">
+    <p class="sa-addwidget__icon" :class="{ 'show-icons': showIcons }">add</p>
+    <div class="sa-addwidget__div" :class="{ 'show-icons': showIcons }">
+      <p class="sa-addwidget__icon">captive_portal</p>
+      <p class="sa-addwidget__icon">checklist</p>
+      <p class="sa-addwidget__icon">event</p>
+      <p class="sa-addwidget__icon">format_paint</p>
+    </div>
     <p class="sa-addwidget__text">Create a new widget</p>
   </button>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showIcons = ref(false);
+</script>
 
 <style lang="scss">
 .sa-addwidget {
@@ -15,12 +28,28 @@
   border: 4px dashed var(--c-fg-tl);
   border-radius: 8px;
   color: var(--c-fg-tl);
+  position: relative;
+}
+
+.sa-addwidget__div {
+  display: none;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+.sa-addwidget__div.show-icons {
+  display: flex;
 }
 
 .sa-addwidget__icon {
   font-family: 'Material Symbols Outlined';
   font-size: 48px;
-  margin-top: 0;
+  margin: 0;
+}
+
+.sa-addwidget__icon.show-icons {
+  display: none;
 }
 
 .sa-addwidget__text {
