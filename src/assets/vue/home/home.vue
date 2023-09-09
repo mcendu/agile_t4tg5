704e16dd526dc4d8b050fcacb4f930fa60560f95
Nathan Du <nathandu@outlook.com>
2023-09-09 19:45:38 +0800

    add skeleton home page

--- src/assets/vue/home/home.vue
+++ src/assets/vue/home/home.vue
@@ -1,11 +1,24 @@
+<script setup lang="ts"></script>
+
 <template>
   <article class="sa-home">
-    <p>This is the home page.</p>
+    <section class="sa-courses sa-current-courses">
+      <h3>Currently taking</h3>
+    </section>
   </article>
 </template>
 
 <style lang="scss">
 .sa-home {
   padding: 1em;
+}
+
+.sa-courses {
+  background-color: var(--c-bg);
+  border-radius: 4px;
+  box-shadow: var(--shadow);
+  margin-bottom: 8px;
+
+  padding: 10px;
 }
 </style>
