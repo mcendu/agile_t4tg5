b44094082c9f347a8ad91b08f07ccace88da523d
Nathan Du <nathandu@outlook.com>
2023-09-11 21:00:31 +0800

    implement module selection

---
+++ src/assets/vue/home/edit.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { ref, Ref, inject, onBeforeMount, computed } from 'vue';
+import '../../css/form.scss';
+import { enabledModulesKey } from '../../js/module';
+import Module from '../../../models/module';
+import FormState from '../../js/composables/formstate';
+
+const enabledModules = inject(enabledModulesKey);
+const allModules = ref<Module[]>([]);
+const dialog: Ref<HTMLDialogElement | undefined> = ref(undefined);
+
+onBeforeMount(
+  async () => (allModules.value = await controllers.module.index()),
+);
+
+const form = new FormState(
+  computed({
+    get() {
+      return allModules.value;
+    },
+    set(modules) {
+      modules.forEach((m, i) => {
+        // assume that the number of modules does not change
+        if (m.enabled != allModules.value[i].enabled)
+          controllers.module.toggle(m.id, m.enabled);
+      });
+      allModules.value = modules;
+    },
+  }),
+);
+
+function showModal() {
+  form.reset();
+  dialog.value?.showModal();
+}
+
+defineExpose({ showModal });
+</script>
+<template>
+  <dialog class="sa-dialog sa-module-select" ref="dialog">
+    <form method="dialog">
+      <h3 class="sa-form-heading">Module selection</h3>
+      <ul class="sa-module-select__list">
+        <li v-for="m in form.data">
+          <label>
+            <input type="checkbox" v-model="m.enabled" />
+            <span>{{ m.code }} &ndash; {{ m.name }}</span>
+          </label>
+        </li>
+      </ul>
+      <p class="sa-form-actions">
+        <button
+          class="form-button submit"
+          type="submit"
+          @click="() => form.save()"
+        >
+          Save
+        </button>
+        <button class="form-button" @click="() => dialog?.close()">
+          Cancel
+        </button>
+      </p>
+    </form>
+  </dialog>
+</template>
--- src/assets/vue/home/home.vue
+++ src/assets/vue/home/home.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-import { inject, ref, Ref } from 'vue';
-import Module, { enabledModulesKey } from '../../js/module';
+import { inject, ref } from 'vue';
+import { enabledModulesKey } from '../../js/module';
+import ModuleSelect from './edit.vue';
 
-const enabledModules: Ref<Module[]> | undefined = inject(enabledModulesKey);
+const enabledModules = inject(enabledModulesKey);
+const moduleSelect = ref<InstanceType<typeof ModuleSelect> | undefined>(
+  undefined,
+);
+
+function showModuleSelectDialog() {
+  if (moduleSelect.value?.showModal) moduleSelect.value.showModal();
+}
 </script>
 
 <template>
@@ -12,6 +20,17 @@ const enabledModules: Ref<Module[]> | undefined = inject(enabledModulesKey);
       <ul>
         <li v-for="m in enabledModules">{{ m.code }} {{ m.name }}</li>
       </ul>
+      <div class="sa-courses__actions">
+        <button class="icon-button sa-courses__action">
+          <span
+            class="material-symbols-outlined"
+            @click="showModuleSelectDialog"
+          >
+            edit
+          </span>
+        </button>
+      </div>
+      <ModuleSelect ref="moduleSelect" />
     </section>
   </article>
 </template>
@@ -22,11 +41,37 @@ const enabledModules: Ref<Module[]> | undefined = inject(enabledModulesKey);
 }
 
 .sa-courses {
+  position: relative;
   background-color: var(--c-bg);
   border-radius: 4px;
   box-shadow: var(--shadow);
   margin-bottom: 8px;
 
   padding: 10px;
+
+  &__actions {
+    position: absolute;
+    right: 2px;
+    top: 2px;
+    z-index: 5;
+
+    display: flex;
+  }
+
+  &__action {
+    padding: 2px;
+    border-radius: 4px;
+  }
+
+  @media (hover: hover) {
+    &__action {
+      opacity: 0;
+    }
+
+    &:hover &__action,
+    &:focus-within &__action {
+      opacity: 1;
+    }
+  }
 }
 </style>
