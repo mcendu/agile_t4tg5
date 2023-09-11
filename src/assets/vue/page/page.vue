18974fdf677cb99ab1e0af0568fa71a9c236777e
Lee Hetherington <67209205+Leery2495@users.noreply.github.com>
2023-09-11 18:34:53 +0100

    Update page.vue

--- src/assets/vue/page/page.vue
+++ src/assets/vue/page/page.vue
@@ -1,137 +1,73 @@
 <script setup lang="ts">
+import MobileHeader from './header.vue';
 import HomePage from '../home/home.vue';
 import GradesPage from '../grades/grades.vue';
 import AddWidget from './addwidget.vue';
-
-import Page from '../../js/page';
-import Widget from '../../../models/widget';
-import widgetTable from '../widgets/index';
+	@@ -10,8 +9,6 @@ import widgetTable from '../widgets/index';
 import { Ref, onBeforeMount, ref, watch } from 'vue';
-
 const props = defineProps<{ page?: Page }>();
+const emit = defineEmits<{ menu: [] }>();
 const widgets: Ref<Widget[] | undefined> = ref(undefined);
-
 function getWidget(type: string) {
-  if (type in widgetTable) {
-    return widgetTable[type];
-  }
-  console.warn(
-    `An unknown widget of type "${type}" is found. The widget is not rendered.`,
-  );
-  return;
-}
-
-async function loadPage() {
-  if (props.page === undefined) {
-    return;
-  }
-
-  widgets.value = await controllers.page.show(props.page.id);
-}
-
-async function updateWidget(w: Widget, data: object) {
-  await controllers.widget.edit(w.id, data);
-  w.data = data;
-}
-
-async function addWidget() {
-  if (props.page === undefined) {
-    return;
-  }
-
-  const w: Widget = {
-    id: -1n,
-    type: 'LinkWidget',
-    data: {
-      title: 'New link',
-      target: 'https://example.com/',
-    },
-  };
-  w.id = (await controllers.widget.add(props.page.id, w)).id;
-  widgets.value?.push(w);
-}
-
-async function deleteWidget(w: Widget) {
-  if (!widgets.value) return;
-  const index = widgets.value.indexOf(w);
-  if (index == -1) return;
-
-  await controllers.widget.del(w.id);
-  widgets.value.splice(index, 1);
-}
-
-onBeforeMount(loadPage);
-watch(() => props.page?.id, loadPage);
+	@@ -68,56 +65,49 @@ watch(() => props.page?.id, loadPage);
 </script>
 
 <template>
-  <main class="sa-page sa-nopages" v-if="page === undefined">
-    <p class="sa-nopages__text">No pages</p>
-    <p class="sa-nopages__tip">
-      Click &ldquo;Add a page&rdquo; on the left to create one.
-    </p>
-  </main>
-  <main class="sa-page sa-special-page" v-else-if="page.special === true">
-    <HomePage v-if="page.name == 'Home'" @reload="loadPage" />
-    <GradesPage v-else-if="page.name == 'Grades'" />
-  </main>
-  <main class="sa-page sa-content" v-else>
-    <component
-      v-for="w of widgets"
-      :is="getWidget(w.type)"
-      :data="w.data"
-      @update="(data: object) => updateWidget(w, data)"
-      @delete="() => deleteWidget(w)"
-    />
-    <AddWidget @click="addWidget" />
+  <main class="sa-page">
+    <MobileHeader :page="page" @menu="$emit('menu')" />
+    <article class="sa-nopages" v-if="page === undefined">
+      <p class="sa-nopages__text">No pages</p>
+      <p class="sa-nopages__tip">
+        Click &ldquo;Add a page&rdquo; on the left to create one.
+      </p>
+    </article>
+    <template v-else-if="page.special === true">
+      <HomePage v-if="page.name == 'Home'" @reload="loadPage" />
+      <GradesPage v-else-if="page.name == 'Grades'" />
+    </template>
+    <article class="sa-content" v-else>
+      <component
+        v-for="w of widgets"
+        :is="getWidget(w.type)"
+        :data="w.data"
+        @update="(data: object) => updateWidget(w, data)"
+        @delete="() => deleteWidget(w)"
+      />
+      <AddWidget @click="addWidget" />
+    </article>
   </main>
 </template>
 
 <style lang="scss">
 @use '/css/stops';
-
 .sa-page {
+  display: flex;
+  flex-direction: column;
   height: 100vh;
+  overflow: hidden;
   background-color: var(--c-b1);
   grid-area: content;
-
-  overflow-y: auto;
 }
-
 .sa-content {
+  height: 100vh;
   display: grid;
   grid-template-rows: repeat(2, 1fr);
   grid-auto-flow: column;
   grid-auto-columns: 12em;
   gap: 8px;
-
   padding: 1em;
-
-  @media (height > stops.$height-s) {
+  overflow: auto;
+  @media (height >= stops.$height-s) {
     grid-auto-columns: 16em;
   }
-
-  @media (stops.$height-m <= height < stops.$height-l) {
-    grid-template-rows: repeat(3, 1fr);
-  }
-
-  @media (height >= stops.$height-l) {
-    grid-template-rows: repeat(4, 1fr);
-  }
-
+	@@ -132,10 +122,14 @@ watch(() => props.page?.id, loadPage);
   @media (width < stops.$width-xs) {
     grid-auto-columns: calc(100vw - 2em);
   }
+  @media (width < stops.$width-s) {
+    height: calc(100vh - 40px);
+  }
 }
-
-.sa-special-page {
-  padding: 1em;
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  justify-content: inherit;
-}
-
 .sa-nopages {
   display: flex;
   flex-direction: column;
